9d29a7e0548ef45ff0ea6e92916d764fde4dabf5
JaWa- <JaWa-@DESKTOP-PN2SJ4E.Speedport_W_724V_Typ_A_05011603_06_003>
2020-04-01 14:21:37 +0200

    test

---
+++ Server + Client/src/Client/Bilder.java
@@ -0,0 +1,18 @@
+package Client;
+
+import java.io.Serializable;
+
+public class Bilder extends Nachricht implements Serializable
+{
+
+	//Variable für Bilder 
+	//Testkommentar
+	
+	//hallo das ist ein test
+	public Bilder(String name, String nachricht)
+	{
+		super(name, nachricht);
+		// TODO Auto-generated constructor stub
+	}
+	
+}
